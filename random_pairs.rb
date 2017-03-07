a91bf508b26776a227b8d2a6b46555a723c00d65
michaelciletti <michaelciletti@gmail.com>
2017-03-07 09:35:06 -0500

    attempting changes to function to get passing test for odd number

--- random_pairs.rb
+++ random_pairs.rb
@@ -9,6 +9,9 @@ end
 def shuffled_names(array)
  	shuffled = array.shuffle.each_slice(2).to_a
 	shuffled.count
+	if array.length == 1
+#trying to figure out how to code if array contains only one string then add the single string into the last pairing
+	end
 end
 
 # classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
