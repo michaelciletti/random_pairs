21040de512dc73c387b11b4b507bc8fee8c8c0f4
michaelciletti <michaelciletti@gmail.com>
2017-03-07 09:12:29 -0500

    tests now pass now need to figure out what to do when odd number

--- random_pairs.rb
+++ random_pairs.rb
@@ -3,12 +3,19 @@ def random(array)
 end
 
 def rpairs(array)
-
 array.shuffle.each_slice(2).to_a
 end
 
-classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
+def shuffled_names(array)
+ 	shuffled = array.shuffle.each_slice(2).to_a
+	shuffled.count
+end
 
+classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
 print rpairs(classmates)
+
+
+
+
 
 
