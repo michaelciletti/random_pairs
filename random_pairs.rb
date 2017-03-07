258b77015af4c6768b6a5d4747c609f9deb4cfa5
michaelciletti <michaelciletti@gmail.com>
2017-03-07 10:26:52 -0500

    trying to deal with the odd number with an iteration

--- random_pairs.rb
+++ random_pairs.rb
@@ -8,21 +8,21 @@ end
 
 def shuffled_names(array)
  	shuffled = array.shuffle.each_slice(2).to_a
-	shuffled.count
-	
-#trying to figure out how to code if array contains only one string then add the single string into the last pairing
-	
-end
+	shuffled.count	
+end  #works for even number of strings in an array but not odd
 
-def shuffled_names(array)
+def shuffled_names_odd(array)  #Trying to figure out how to add odd man out into a pairing
  	shuffled = array.shuffle.each_slice(2).to_a
-	shuffled.count
 	shuffled.each do |pair|
+		if pair == 1
+		+ pair.last
+		end
+	shuffled.count
 	end
 end
 
-# classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
-# print rpairs(classmates)
+classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
+print shuffled_names_odd(classmates)
 
 
 
