136c8adc99aecd314c4a497d527d4eafb5df92cd
michaelciletti <michaelciletti@gmail.com>
2017-03-07 20:29:14 -0500

    final

--- random_pairs.rb
+++ random_pairs.rb
@@ -7,22 +7,28 @@ def rpairs(array)
 end
 
 def shuffled_names(array)
- 	shuffled = array.shuffle.each_slice(2).to_a
+	shuffled = array.shuffle.each_slice(2).to_a
 	shuffled.count	
+	# shuffled	
 end  #works for even number of strings in an array but not odd
+array = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
+# puts shuffled_names(array)
+
 
 def shuffled_names_odd(array)  #Trying to figure out how to add odd man out into a pairing
  	shuffled = array.shuffle.each_slice(2).to_a
 	shuffled.each do |pair|
-	# 	if pair == 1
-	# 	+ pair.last
+		if pair.count == 1
+	 		shuffled[0].push(pair[0])
+			shuffled.pop
 		end
+  	end
 	shuffled.count
-	# end
+	# shuffled
 end
 
-classmates = ["Mike","Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
-print shuffled_names(classmates)
+classmates = ["Mike", "Bob", "Joe", "Lisa","Andrew","Khalifa","Cummie", "Jayvon", "Takir"]
+# print shuffled_names_odd(classmates)
 
 
 
